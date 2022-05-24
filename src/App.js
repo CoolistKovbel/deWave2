a8c7fd6dbe6817632fcaa4d2920bd8f88e9aaf95
coolistkovbel <coolistkovbel@gmail.com>
2022-05-24 00:24:12 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import { ethers } from "ethers";
 
-function App() {
+
+export default function App () {
+
+  // State Variables
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [validAccount, setValidAccount] = useState(false);
+
+
+  const checkIfWalletIsConnect = async () => {
+    try {
+      // Make sure we have access to window.ethereum
+      const {ethereum} = window
+
+      if(!ethereum){
+        alert("Get metamask")
+      } else {
+        console.log("Ethereum object exists: ", ethereum)
+      }
+
+      // Check to if authorized User
+      const accounts = await ethereum.request({method: "eth_accounts"});
+
+      if(accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Authorized account: ", account)
+        setValidAccount(true)
+        setCurrentAccount(account)
+      } else {
+        console.log("no authorized account")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnect()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h2>Welcome to the Geto Wave App</h2>
+      <div>
+        <h2>Profile Account: {currentAccount}</h2>
+      </div>
+      <button className="waveButton" onClick={null}>
+          Wave at Me
+      </button>
 
-export default App;
+    </div>
+  )
+}
